e03ca4cfeb7e678bde5a2cee91734c7a061aefcb
Roopesh <62459820+Roopesh2@users.noreply.github.com>
2020-09-29 07:53:18 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,19 +1,20 @@
 (function(){
     function int (a, b) { return isNaN(a) ? b: a; }
-
     function bool (a, b) { return a == undefined ? b : a; }
+    
     function gW () {
-        var cs = window.getComputedStyle(document.body);
-        return window.innerWidth - parseInt(cs.marginLeft) * 2;
+      var cs = window.getComputedStyle(document.body);
+      return window.innerWidth - parseInt(cs.marginLeft) * 2;
     }
+    
     function ACE (extObj) {
-        window.CJS_Extensions = Object.assign(window.CJS_Extensions, extObj);
+      window.CJS_Extensions = Object.assign(window.CJS_Extensions, extObj);
     }
 
     assignPropsToWind({
-        int: int,
-        bool: bool,
-        getWidth: gW,
-        addCJSExtension: ACE
+      int: int,
+      bool: bool,
+      getWidth: gW,
+      addCJSExtension: ACE
     });
-})()
+  })();
