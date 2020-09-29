8537aa6fe0b4446a5da32e882208eceacea66914
Roopesh <62459820+Roopesh2@users.noreply.github.com>
2020-09-29 07:52:40 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -64,49 +64,47 @@ function assignPropsToWind(obj){
     ORANGE      : "#FF862F",
     NONE        : "transparent"
   };
-
   window.COLOR_LIST = CList;
   for (var color of Object.keys(CList)) {
-      window[color] = CList[color];
+    window[color] = CList[color];
   }
   window.CJS_Extensions = {};
+
+  // constant functions
+  assignPropsToWind({
+    E: Math.E,
+    LN2: Math.LN2,
+    LN10: Math.LN10,
+    PI: Math.PI,
+    SQRT2: Math.SQRT2,
+    abs: Math.abs,
+    acos: Math.acos,
+    asin: Math.asin,
+    atan: Math.atan,
+    atan2: Math.atan2,
+    cbrt: Math.cbrt,
+    ceil: Math.ceil,
+    cos: Math.cos,
+    cosh: Math.cosh,
+    exp: Math.exp,
+    floor: Math.floor,
+    log: Math.log,
+    log2: Math.log2,
+    log10: Math.log10,
+    max: Math.max,
+    min: Math.min,
+    pow: Math.pow,
+    rand: Math.random,
+    round: Math.round,
+    sgn: Math.sign,
+    sin: Math.sin,
+    sqrt: Math.sqrt,
+    tan: Math.tan,
+    tanh: Math.tanh,
+    dist : function(p1, p2) {
+      if (p1 instanceof Array && p2 instanceof Array) {
+        return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2);
+      }
+    }
+  });
 })();
-
-// constant functions
-
-const {
-    E,
-    LN2,
-    LN10,
-    PI,
-    SQRT2,
-    abs,
-    acos,
-    asin,
-    atan,
-    atan2,
-    cbrt,
-    ceil,
-    cos,
-    cosh,
-    exp,
-    floor,
-    log,
-    log2,
-    log10,
-    max,
-    min,
-    pow,
-    random: rand,
-    round,
-    sign: sgn,
-    sin,
-    sqrt,
-    tan,
-    tanh,
-} = Math,
-    dist = function(p1, p2) {
-        if (p1 instanceof Array && p2 instanceof Array) {
-            return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2);
-        }
-    };
