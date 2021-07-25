046c7671f875284a7fefb7e5c3d233f1dbfedb0b
Roopesh <roopeshor6@gmail.com>
2021-07-25 16:34:31 +0530

    Added more function

--- src/color/interpolation.js
+++ src/color/interpolation.js
@@ -7,7 +7,7 @@ import { readColor } from "./color_reader.js";
  * @param {string} color2 color
  * @param {number} v should be between 0 and 1.
  */
-function lerpColor (color1, color2, v) {
+function lerpColor(color1, color2, v) {
 	const c1 = readColor([color1], true);
 	const c2 = readColor([color2], true);
 	return readColor([
@@ -18,6 +18,24 @@ function lerpColor (color1, color2, v) {
 	]);
 }
 
-export {
-	lerpColor
-};
+function lerpColorArray(colorArr, v) {
+	const stopes = Object.keys(colorArr).sort();
+	const min = Math.min(...stopes);
+	const max = Math.max(...stopes);
+	if (v >= max) return colorArr[max];
+	if (v <= min) return colorArr[min];
+	for (var i = 0; i < stopes.length - 1; i++) {
+		let a = stopes[i];
+		if (v > a) {
+			return lerpColor(
+				colorArr[a],
+				colorArr[stopes[i + 1]],
+				(v - a) / (stopes[i + 1] - a)
+			);
+		} else if (v == a) {
+			return colorArr[a];
+		}
+	}
+}
+
+export { lerpColor, lerpColorArray };
